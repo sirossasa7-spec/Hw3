d2bc4d3fb0360cfb0b96d633ffdc8fe3d5787a1e
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-21 23:06:30 +0200

    Add project files.

---
+++ Hw-3/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Hw_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Website site = new Website();
+
+            site.Input();
+
+            Console.WriteLine("\nІнформація про сайт:");
+            Console.WriteLine(site);
+        }
+    }
+    class Website
+    {
+        private string name;
+        private string path;
+        private string description;
+        private string url;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+
+        public void Input()
+        {
+            Console.Write("Назва сайту: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Шлях до сайту: ");
+            Path = Console.ReadLine();
+
+            Console.Write("Опис сайту: ");
+            Description = Console.ReadLine();
+
+            Console.Write("Адреса сайту: ");
+            Url = Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            return $"Назва: {Name}\n" +
+                   $"Шлях: {Path}\n" +
+                   $"Опис: {Description}\n" +
+                   $"Адреса: {Url}";
+        }
+    }
+}
---
+++ Hw-3/Task2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw_3
+{
+    internal class Task2
+    {
+    }
+}
